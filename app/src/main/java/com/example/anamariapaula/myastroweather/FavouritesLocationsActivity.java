7662c083c5e2fb5aeff9082c415cd0dfef233927
Vladyslav <187185@edu.p.lodz.pl>
2017-09-05 00:39:33 +0200

    Zaczęte Activity Favourites Locations

---
+++ app/src/main/java/com/example/anamariapaula/myastroweather/FavouritesLocationsActivity.java
@@ -0,0 +1,43 @@
+package com.example.anamariapaula.myastroweather;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FavouritesLocationsActivity extends AppCompatActivity {
+
+    Spinner spinner_favourites_locations;
+    Spinner spinner_founded_locations;
+
+    EditText editText_find_location;
+
+    Button button_find_location;
+    Button button_add_to_favourites;
+    Button button_remove_from_favourites;
+
+    ArrayList<Location> favouritesLocations;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_favourites_locations);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        spinner_favourites_locations = (Spinner) findViewById(R.id.spinner_favourites_locations);
+        spinner_founded_locations = (Spinner) findViewById(R.id.spinner_founded_locations);
+        editText_find_location = (EditText) findViewById(R.id.editText_find_location);
+        button_find_location = (Button) findViewById(R.id.button_find_location);
+        button_add_to_favourites = (Button) findViewById(R.id.button_add_to_favourites);
+        button_remove_from_favourites = (Button) findViewById(R.id.button_remove_from_favourites);
+    }
+}
